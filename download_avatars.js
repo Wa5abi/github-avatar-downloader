94f956d18db71ce7e88f9943fac80a99f0d95e25
Dene <W4548147gm411@gmail.com>
2017-08-14 18:36:57 +0000

    added a function to make a request to the github api

---
+++ download_avatars.js
@@ -0,0 +1,15 @@
+var request = require('request');
+var GITHUB_USER = "Wa5abi";
+var GITHUB_TOKEN = "01b8152bd4507278db846b201978b498b0e3d42c";
+
+console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
